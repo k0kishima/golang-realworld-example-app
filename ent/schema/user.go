77f05fea77ec23e7c566cf971ff1ece7417e5884
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-03-06 20:22:43 +0900

    Add generated ent files for User schema

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -1,18 +1,33 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
 
-// User holds the schema definition for the User entity.
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"github.com/google/uuid"
+)
+
 type User struct {
 	ent.Schema
 }
 
-// Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Unique(),
+		field.String("username").Unique().NotEmpty(),
+		field.String("email").Unique().NotEmpty(),
+		field.String("password").NotEmpty(),
+		field.String("image").Default("https://avatars.githubusercontent.com/u/32737308?v=4"),
+		field.String("bio").Default(""),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now),
+	}
 }
 
-// Edges of the User.
-func (User) Edges() []ent.Edge {
-	return nil
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("username", "email").Unique(),
+	}
 }
