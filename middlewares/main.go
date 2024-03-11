b5d4b3612c62e730c949f4fa84c315fcd5f42a29
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-03-11 21:23:27 +0900

    Implement CORS middleware

---
+++ middlewares/main.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		allowedOrigins := strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+		origin := c.Request.Header.Get("Origin")
+
+		if contains(allowedOrigins, origin) {
+			c.Header("Access-Control-Allow-Origin", origin)
+		} else {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Origin not allowed"})
+			return
+		}
+
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
+	}
+	return false
+}
